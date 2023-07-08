82e0262df6746e0a02db0d49707971e186aa13d4
GBondoc <138961965+GBondoc@users.noreply.github.com>
2023-07-08 13:35:55 +0300

    Creating the project

---
+++ src/NavigateDB/src/main/java/com/navigatedb/ws/NavigateDbApplication.java
@@ -0,0 +1,13 @@
+package com.navigatedb.ws;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class NavigateDbApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(NavigateDbApplication.class, args);
+    }
+
+}
---
+++ src/NavigateDB/src/test/java/com/navigatedb/ws/NavigateDbApplicationTests.java
@@ -0,0 +1,13 @@
+package com.navigatedb.ws;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class NavigateDbApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
