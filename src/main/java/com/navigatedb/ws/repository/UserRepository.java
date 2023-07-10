e8a8d0613a8b66ca006455b8bc7b8f3f85c1b31b
GBondoc <138961965+GBondoc@users.noreply.github.com>
2023-07-10 14:08:58 +0300

    Added Get all users method with pagination

--- src/main/java/com/navigatedb/ws/repository/UserRepository.java
+++ src/main/java/com/navigatedb/ws/repository/UserRepository.java
@@ -2,10 +2,11 @@ package com.navigatedb.ws.repository;
 
 import com.navigatedb.ws.io.entity.UserEntity;
 import org.springframework.data.repository.CrudRepository;
+import org.springframework.data.repository.PagingAndSortingRepository;
 import org.springframework.stereotype.Repository;
 
 @Repository
-public interface UserRepository extends CrudRepository<UserEntity, Long> {
+public interface UserRepository extends PagingAndSortingRepository<UserEntity, Long>, CrudRepository<UserEntity, Long> {
     UserEntity findByEmail(String email);
     UserEntity findByUsername(String username);
     UserEntity findByUserId(String userId);
--- src/main/java/com/navigatedb/ws/service/UserService.java
+++ src/main/java/com/navigatedb/ws/service/UserService.java
@@ -2,10 +2,13 @@ package com.navigatedb.ws.service;
 
 import com.navigatedb.ws.shared.dto.UserDto;
 
+import java.util.List;
+
 
 public interface UserService {
     UserDto createUser(UserDto user);
     UserDto getUserByUserId(String userId);
     UserDto updateUser(String userId, UserDto user);
     void deleteUser(String userId);
+    List<UserDto> getUsers(int page, int limit);
 }
