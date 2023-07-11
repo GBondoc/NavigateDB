6ca3dd8c77b6a09e2d23debe2cf6b5bc27b3bfe0
GBondoc <138961965+GBondoc@users.noreply.github.com>
2023-07-11 16:43:36 +0300

    Moved secret token to application properties file

--- src/main/java/com/navigatedb/ws/security/SecurityConstants.java
+++ src/main/java/com/navigatedb/ws/security/SecurityConstants.java
@@ -1,11 +1,17 @@
 package com.navigatedb.ws.security;
 
+import com.navigatedb.ws.SpringApplicationContext;
+import org.springframework.core.env.Environment;
+
 public class SecurityConstants {
 
     public static final long EXPIRATION_TIME = 864000000; // 10 days
     public static final String TOKEN_PREFIX = "Bearer ";
     public static final String HEADER_STRING = "Authorization";
     public static final String SIGN_UP_URL = "/users";
-    public static final String TOKEN_SECRET = "b3Ks8d2Jv6zL1yQr4MxH7sN0aP9fXcVt5EwZiG6uYqPlWnRoTpO4jAm5Kl8Uv3FnCs9Vd1BgNmLzIxHkS6TcR8YpQo9WfZrM0C";
 
+    public static String getTokenSecret() {
+        Environment environment = (Environment) SpringApplicationContext.getBean("environment");
+        return environment.getProperty("tokenSecret");
+    }
 }
