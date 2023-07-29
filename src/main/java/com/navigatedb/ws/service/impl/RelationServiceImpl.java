b763c15cb053195ee35992103f8bce4c0d3edbc1
GBondoc <138961965+GBondoc@users.noreply.github.com>
2023-07-29 15:03:35 +0300

    Implemented CRUD for relations

--- src/main/java/com/navigatedb/ws/service/impl/RelationServiceImpl.java
+++ src/main/java/com/navigatedb/ws/service/impl/RelationServiceImpl.java
@@ -1,8 +1,79 @@
 package com.navigatedb.ws.service.impl;
 
+import com.navigatedb.ws.exceptions.RelationServiceException;
+import com.navigatedb.ws.io.entity.RelationEntity;
+import com.navigatedb.ws.repository.RelationRepository;
 import com.navigatedb.ws.service.RelationService;
+import com.navigatedb.ws.shared.Utils;
+import com.navigatedb.ws.shared.dto.RelationDto;
+import com.navigatedb.ws.ui.model.response.ErrorMessages;
+import org.modelmapper.ModelMapper;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 @Service
 public class RelationServiceImpl implements RelationService {
+
+    @Autowired
+    RelationRepository relationRepository;
+
+    @Autowired
+    Utils utils;
+
+    @Override
+    public RelationDto createRelation(RelationDto relation) {
+
+        if((relationRepository.findByRelationType(relation.getRelationType()) != null)
+        && (relationRepository.findByNullable(relation.getNullable()) != null))
+            throw new RelationServiceException(ErrorMessages.RECORD_ALREADY_EXISTS.getErrorMessage());
+
+        ModelMapper modelMapper = new ModelMapper();
+        RelationEntity relationEntity = modelMapper.map(relation, RelationEntity.class);
+
+        String publicRelationId = utils.generateRelationId(30);
+        relationEntity.setRelationId(publicRelationId);
+
+        RelationEntity storedRelationDetails = relationRepository.save(relationEntity);
+
+        return modelMapper.map(storedRelationDetails, RelationDto.class);
+    }
+
+    @Override
+    public RelationDto getRelationByRelationId(String relationId) {
+
+        RelationEntity relationEntity = relationRepository.findByRelationId(relationId);
+
+        if(relationEntity == null) throw new RelationServiceException("Relation with ID: " + relationId + " not found");
+
+        ModelMapper modelMapper = new ModelMapper();
+
+        return modelMapper.map(relationEntity, RelationDto.class);
+    }
+
+    @Override
+    public RelationDto updateRelation(String relationId, RelationDto relation) {
+        RelationEntity relationEntity = relationRepository.findByRelationId(relationId);
+
+        if(relationEntity == null)
+            throw new RelationServiceException(ErrorMessages.NO_RECORD_FOUND.getErrorMessage());
+
+        relationEntity.setRelationType(relation.getRelationType());
+        relationEntity.setNullable(relation.getNullable());
+
+        RelationEntity updatedRelationDetails = relationRepository.save(relationEntity);
+
+        ModelMapper modelMapper = new ModelMapper();
+
+        return modelMapper.map(updatedRelationDetails, RelationDto.class);
+    }
+
+    @Override
+    public void deleteRelation(String relationId) {
+        RelationEntity relationEntity = relationRepository.findByRelationId(relationId);
+
+        if(relationEntity == null)
+            throw new RelationServiceException(ErrorMessages.NO_RECORD_FOUND.getErrorMessage());
+
+        relationRepository.delete(relationEntity);
+    }
 }
--- src/main/java/com/navigatedb/ws/ui/controller/RelationController.java
+++ src/main/java/com/navigatedb/ws/ui/controller/RelationController.java
@@ -1,9 +1,105 @@
 package com.navigatedb.ws.ui.controller;
 
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import com.navigatedb.ws.exceptions.RelationServiceException;
+import com.navigatedb.ws.service.RelationService;
+import com.navigatedb.ws.shared.dto.RelationDto;
+import com.navigatedb.ws.ui.model.request.RelationDetailsRequestModel;
+import com.navigatedb.ws.ui.model.response.ErrorMessages;
+import com.navigatedb.ws.ui.model.response.OperationStatusModel;
+import com.navigatedb.ws.ui.model.response.RelationRest;
+import com.navigatedb.ws.ui.model.response.RequestOperationStatus;
+import org.modelmapper.ModelMapper;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.MediaType;
+import org.springframework.web.bind.annotation.*;
 
 @RestController
 @RequestMapping("relations")
 public class RelationController {
+
+    @Autowired
+    RelationService relationService;
+
+
+    @GetMapping(path = "/{id}", produces = { MediaType.APPLICATION_XML_VALUE,
+            MediaType.APPLICATION_JSON_VALUE})
+    public RelationRest getRelation(@PathVariable String id) {
+
+        RelationDto relationDto = relationService.getRelationByRelationId(id);
+        ModelMapper modelMapper = new ModelMapper();
+
+        return modelMapper.map(relationDto, RelationRest.class);
+    }
+
+    @PostMapping(
+            consumes = {MediaType.APPLICATION_XML_VALUE, MediaType.APPLICATION_JSON_VALUE},
+            produces = { MediaType.APPLICATION_XML_VALUE, MediaType.APPLICATION_JSON_VALUE}
+    )
+    public RelationRest createRelation(@RequestBody RelationDetailsRequestModel relationDetails) {
+
+        if(relationDetails.getRelationType().isEmpty() || relationDetails.getNullable().isEmpty())
+            throw new RelationServiceException(ErrorMessages.MISSING_REQUIRED_FIELD.getErrorMessage());
+
+        ModelMapper modelMapper = new ModelMapper();
+        RelationDto relationDto = modelMapper.map(relationDetails, RelationDto.class);
+
+        RelationDto createdRelation = relationService.createRelation(relationDto);
+
+        return modelMapper.map(createdRelation, RelationRest.class);
+    }
+
+    @PutMapping(path = "/{id}",
+            consumes = {MediaType.APPLICATION_XML_VALUE, MediaType.APPLICATION_JSON_VALUE},
+            produces = { MediaType.APPLICATION_XML_VALUE, MediaType.APPLICATION_JSON_VALUE}
+    )
+    public RelationRest updateRelation(@PathVariable String id,
+                                       @RequestBody RelationDetailsRequestModel relationDetails) {
+
+        if(relationDetails.getRelationType().isEmpty()
+        || relationDetails.getNullable().isEmpty())
+            throw new RelationServiceException(ErrorMessages.MISSING_REQUIRED_FIELD.getErrorMessage());
+
+        ModelMapper modelMapper = new ModelMapper();
+        RelationDto relationDto = modelMapper.map(relationDetails, RelationDto.class);
+
+        RelationDto updatedRelation = relationService.updateRelation(id, relationDto);
+
+        return modelMapper.map(updatedRelation, RelationRest.class);
+    }
+
+    @DeleteMapping(path = "/{id}",
+            produces = { MediaType.APPLICATION_XML_VALUE, MediaType.APPLICATION_JSON_VALUE}
+    )
+    public OperationStatusModel deleteRelation(@PathVariable String id) {
+
+        OperationStatusModel returnValue = new OperationStatusModel();
+        returnValue.setOperationName(RequestOperationName.DELETE.name());
+
+        relationService.deleteRelation(id);
+
+        returnValue.setOperationResult(RequestOperationStatus.SUCCESS.name());
+
+        return returnValue;
+
+    }
+
 }
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
