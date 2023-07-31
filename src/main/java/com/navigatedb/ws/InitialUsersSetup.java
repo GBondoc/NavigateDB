3e46bf61378009efe7344202bdd54fe7cf33f40a
GBondoc <138961965+GBondoc@users.noreply.github.com>
2023-07-31 16:13:05 +0300

    Created USER and ADMIN roles

--- src/main/java/com/navigatedb/ws/InitialUsersSetup.java
+++ src/main/java/com/navigatedb/ws/InitialUsersSetup.java
@@ -1,11 +1,17 @@
 package com.navigatedb.ws;
 
 import com.navigatedb.ws.io.entity.AuthorityEntity;
+import com.navigatedb.ws.io.entity.RoleEntity;
 import com.navigatedb.ws.repository.AuthorityRepository;
+import com.navigatedb.ws.repository.RoleRepository;
+import jakarta.transaction.Transactional;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.context.event.ApplicationReadyEvent;
 import org.springframework.context.event.EventListener;
 import org.springframework.stereotype.Component;
+
+import java.util.Arrays;
+import java.util.Collection;
 
 @Component
 public class InitialUsersSetup {
@@ -13,14 +19,22 @@ public class InitialUsersSetup {
     @Autowired
     AuthorityRepository authorityRepository;
 
+    @Autowired
+    RoleRepository roleRepository;
+
     @EventListener
+    @Transactional
     public void onApplicationEvent(ApplicationReadyEvent event) {
 
         AuthorityEntity readAuthority = createAuthority("READ_AUTHORITY");
         AuthorityEntity writeAuthority = createAuthority("WRITE_AUTHORITY");
         AuthorityEntity deleteAuthority = createAuthority("DELETE_AUTHORITY");
+
+        createRole("ROLE_USER", Arrays.asList(readAuthority, writeAuthority));
+        createRole("ROLE_ADMIN", Arrays.asList(readAuthority, writeAuthority, deleteAuthority));
     }
 
+    @Transactional
     private AuthorityEntity createAuthority(String name) {
 
         AuthorityEntity authority = authorityRepository.findByName(name);
@@ -29,6 +43,18 @@ public class InitialUsersSetup {
             authorityRepository.save(authority);
         }
         return authority;
+    }
+
+    @Transactional
+    private RoleEntity createRole(String name, Collection<AuthorityEntity> authorities) {
+
+        RoleEntity role = roleRepository.findByName(name);
+        if(role == null) {
+            role = new RoleEntity(name);
+            role.setAuthorities(authorities);
+            roleRepository.save(role);
+        }
+        return role;
     }
 
 }
