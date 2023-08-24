524cb796f115b37b7f8c112f057c685d873f7788
GBondoc <138961965+GBondoc@users.noreply.github.com>
2023-08-25 00:01:09 +0300

    Erds are now listed from the database for current logged in user

--- src/FE/js/erdListing.js
+++ src/FE/js/erdListing.js
@@ -1,10 +1,47 @@
-const erdGrid = document.querySelector('.erd--grid');
+document.addEventListener("DOMContentLoaded", () => {
+    const erdGrid = document.querySelector('.erd--grid');
+    const errorMessageElement = document.getElementById('error-message');
+    const token = localStorage.getItem('jwtToken');
+    const publicUserId = localStorage.getItem('publicUserId');
 
-const erdNames = ['Database 1', 'Database 2', 'Database 3', 'Database 4', 'Database 5', 'Database 6'];
+    // Function to create an ERD square
+    function createErdSquare(erdName) {
+        const erdSquare = document.createElement('div');
+        erdSquare.classList.add('erd-square');
+        erdSquare.textContent = erdName;
+        erdGrid.appendChild(erdSquare);
+    }
 
-erdNames.forEach(erdName => {
-    const erdSquare = document.createElement('div');
-    erdSquare.classList.add('erd-square');
-    erdSquare.textContent = erdName;
-    erdGrid.appendChild(erdSquare);
+    // Function to display error message
+    function displayErrorMessage(message) {
+        errorMessageElement.textContent = message;
+    }
+
+    // Function to fetch and populate ERDs
+    async function fetchErds() {
+        try {
+            const getErdsRespose = await fetch(`http://localhost:8080/NavigateDB/users/${publicUserId}/erds`, {
+                method: 'GET',
+                headers: {
+                    'Authorization': token,
+                    'Accept': 'application/json',
+                    'Content-Type': 'application/json'
+                }
+            });
+            if (!getErdsRespose.ok) {
+                throw new Error('Error fetching ERDs');
+            }
+            const data = await getErdsRespose.json();
+
+            data.forEach(erdData => {
+                createErdSquare(erdData.name);
+            });
+        } catch (error) {
+            console.error('Error fetching ERDs:', error);
+            displayErrorMessage('An error occurred while fetching ERDs. Please try again later.');
+        }
+    }
+
+    // Fetch and populate ERDs on page load
+    fetchErds();
 });
