76bc406d8474e9909a8f6d409141fae81a3376ce
GBondoc <138961965+GBondoc@users.noreply.github.com>
2023-08-24 22:42:25 +0300

    Added JavaScript blueprint for entity input values

---
+++ src/FE/js/entity.js
@@ -0,0 +1,17 @@
+document.addEventListener("DOMContentLoaded", () => {
+    const entityForm = document.querySelector("#createEntity");
+
+    entityForm.addEventListener("submit", async (e) => {
+        e.preventDefault();
+
+        const entityNameInput = entityForm.querySelector(".form__input[placeholder='Entity Name']");
+        const rowCountInput = entityForm.querySelector(".form__input[placeholder='Row Count']");
+
+        const entityName = entityNameInput.value;
+        const rowCount = rowCountInput.value;
+        const token = localStorage.getItem('jwtToken');
+        const publicUserId = localStorage.getItem('publicUserId');
+
+        
+    });
+});
