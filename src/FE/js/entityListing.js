f78bef8c2d96e6fddc38ed612cc480ccfa5bf621
GBondoc <138961965+GBondoc@users.noreply.github.com>
2023-09-13 23:14:25 +0300

    Revert "Partially implemented entity fetching from back end"
    This reverts commit 6c5b356e414133c8587173c38c34309b4df9d128.

--- src/FE/js/entityListing.js
+++ src/FE/js/entityListing.js
@@ -1,42 +1,12 @@
 document.addEventListener("DOMContentLoaded", () => {
     const entityList = document.querySelector('.list');
-    const errorMessageElement = document.getElementById('error-message');
-    const token = localStorage.getItem('jwtToken');
-    const publicUserId = localStorage.getItem('publicUserId');
-    const selectedErd = localStorage.getItem('erdId');
 
-    console.log(selectedErd);
-    async function fetchEntities() {
-        try {
-            const response = await fetch(`http://localhost:8080/NavigateDB/users/${publicUserId}/erds/${selectedErd}/entities`, {
-                method: 'GET',
-                headers: {
-                    'Authorization': token,
-                    'Accept': 'application/json',
-                    'Content-Type': 'application/json'
-                }
-            });
+    const entityNames = ["Entity 1", "Entity 2", "Entity 3", "Entity 4", "Entity 5"];
 
-            if (!response.ok) {
-                throw new Error('Error fetching entities');
-            }
-
-            const data = await response.json();
-
-            data.forEach(entityData => {
-                const listItem = document.createElement('li');
-                listItem.classList.add('list-entity');
-                listItem.textContent = entityData.name;
-                entityList.appendChild(listItem);
-            });
-        } catch (error) {
-            console.error('Error fetching entities:', error);
-            displayErrorMessage('An error occurred while fetching entities. Please try again later.');
-        }
-    }
-
-    // Call the fetchEntities function to populate entities on page load
-    fetchEntities();
-
-    // Your other code...
+    entityNames.forEach(entityName => {
+        const listItem = document.createElement('li');
+        listItem.classList.add('list-entity');
+        listItem.textContent = entityName;
+        entityList.appendChild(listItem);
+    });
 });
